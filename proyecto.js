f6ec4a44c6e23c9a8ca197a4bdb0ef925958d62f
Vanesa Zarate <vaneezarate@gmail.com>
2022-11-08 21:17:42 -0300

    eliminar

--- proyecto.js
+++ proyecto.js
@@ -5,25 +5,25 @@ const modalContainer = document.getElementById("modal-container");
 const productos = [
     {
         id: 1,
-        nombre: "Milanesa",
+        nombre: "Milanesas de pollo",
         precio: 1050,
         img: "./img/mila.png",
     },
     {
         id: 2,
-        nombre: "Matambre",
+        nombre: "Matambre de carne",
         precio: 850,
         img: "./img/matambre.png",
     },
     {
         id: 3,
-        nombre: "Hamburguesa",
+        nombre: "Hamburguesa de carne",
         precio: 400,
         img:"./img/hamburguesa.png",
     },
     {
         id: 4,
-        nombre: "Bastones",
+        nombre: "Bastones de muzzarella",
         precio: 750,
         img: "./img/bastones.png",
     },
@@ -58,34 +58,64 @@ productos.forEach((product) =>{
 
 });
 
-verCarrito.addEventListener("click", () =>{
+const elegirCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
     modalHeader.className ="modal-header"
     modalHeader.innerHTML = `
-        <h1 class="modal-header-title">Carrito</h1>
+        <h1 class="modal-header-title">Productos elegidos:</h1>
     `;
 
     modalContainer.append(modalHeader);
 
     const modalbutton = document.createElement("h1");
-    modalbutton.innerText = "Eliminar";
+    modalbutton.innerText = "Cerrar";
     modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () =>{
+        modalContainer.style.display = "none";
+    });
 
     modalHeader.append(modalbutton);
 
     carrito.forEach((product) => {
-
-    })
-
-    let carritoContent = document.createElement("div");
-    carritoContent.className = "modal-content";
-    carritoContent.innerHTML = `
-    <img src =${product.img}">
-    <h3>${product.nombre}</h3>
-    <p>$ ${product.precio} <p>
-    `;
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>$ ${product.precio}<p>
+        `;
 
     modalContainer.append(carritoContent);
 
-    const total = carrito.reduce ((acc, el)=> acc + el.precio, 0);
+    let eliminar = document.createElement("span");
+
+    eliminar.innerText = "❌";
+    eliminar.className= "borrar";
+    carritoContent.append(eliminar);
+
+    eliminar.addEventListener("click",eliminarProducto);
+
 });
+
+    const total = carrito.reduce ((acc, el)=> acc + el.precio, 0);
+
+    const totalfinal = document.createElement ("div");
+    totalfinal.className = "total-content";
+    totalfinal.innerHTML =`Total: $ ${total}`;
+    modalContainer.append(totalfinal);
+};
+
+verCarrito.addEventListener("click", elegirCarrito);
+
+const eliminarProducto = () =>{
+    const foundId =carrito.find((element)=> element.id);
+
+    carrito= carrito.filter((carritoId) =>{
+        return carritoId !== foundId;
+    });
+
+    elegirCarrito();
+};
